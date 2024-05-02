7c92be897d10a282e437327b6cb59c28b46331de
SadPastel <128807200+SadPastel@users.noreply.github.com>
2024-05-02 12:18:25 -0300

    Update java.js

---
+++ java.js
@@ -0,0 +1,17 @@
+const nome = "Eliã"
+let segundo_Nome = "Vieira"
+var nome_completo = (nome+segundo_Nome)
+//Eliã Vieira
+
+
+const Nome = "Eliã tem"
+let idade = 17
+var nome_completo = (Nome+idade)
+//Elia 17 
+
+
+const numero1 <= 2  
+let numero2 = 3
+var resposta = (numero1-numero2)
+//2
+
